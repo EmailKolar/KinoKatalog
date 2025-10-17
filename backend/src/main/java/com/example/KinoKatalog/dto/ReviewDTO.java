60ca8b599e0c44398fba7e6f9a26465b96c97d34
emailkolar <emilaalund.nyboder@gmail.com>
2025-10-17 11:47:34 +0200

    ReviewDTO added

---
+++ backend/src/main/java/com/example/KinoKatalog/dto/ReviewDTO.java
@@ -0,0 +1,12 @@
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewDTO {
+    private Integer id;
+    private String username; // from user
+    private Integer movieId; // from movie
+    private Integer rating;
+    private String reviewText;
+    private LocalDateTime createdAt;
+}
