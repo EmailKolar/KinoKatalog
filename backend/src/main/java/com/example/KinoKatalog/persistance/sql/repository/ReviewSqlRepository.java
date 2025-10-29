e46bd26077d43c71dbaee96e581051e71fb7e9f1
emailkolar <emilaalund.nyboder@gmail.com>
2025-10-29 10:03:56 +0100

    MOVIE domain work

--- backend/src/main/java/com/example/KinoKatalog/persistance/sql/repository/ReviewSqlRepository.java
+++ backend/src/main/java/com/example/KinoKatalog/persistance/sql/repository/ReviewSqlRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 
 @Repository
-public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer> {
+public interface ReviewSqlRepository extends JpaRepository<ReviewEntity, Integer> {
     List<ReviewEntity> findByMovieId(Integer movieId);
 }
 
