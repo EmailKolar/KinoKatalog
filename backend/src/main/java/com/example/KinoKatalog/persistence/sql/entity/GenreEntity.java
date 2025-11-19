caa6cbc5d70e12410abaafb3c67492c236addc9e
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2025-11-19 09:15:47 +0100

    Made GenreEntity.java an actual entity

--- backend/src/main/java/com/example/KinoKatalog/persistence/sql/entity/GenreEntity.java
+++ backend/src/main/java/com/example/KinoKatalog/persistence/sql/entity/GenreEntity.java
@@ -2,7 +2,15 @@ package com.example.KinoKatalog.persistence.sql.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "genres")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GenreEntity {
 
 
