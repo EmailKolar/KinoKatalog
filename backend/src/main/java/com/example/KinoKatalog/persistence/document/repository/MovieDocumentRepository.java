7d99ba87bb05faad7caea466540d8a988412bf43
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2025-11-26 09:48:18 +0100

    Made custom implementation of query

--- backend/src/main/java/com/example/KinoKatalog/persistence/document/repository/MovieDocumentRepository.java
+++ backend/src/main/java/com/example/KinoKatalog/persistence/document/repository/MovieDocumentRepository.java
@@ -4,6 +4,13 @@ import com.example.KinoKatalog.persistence.document.documents.MovieDocument;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface MovieDocumentRepository extends MongoRepository<MovieDocument, ObjectId> {
     //TODO: Add custom query methods if needed
+
+
+    @Override
+    List<MovieDocument> findAll();
+    List<MovieDocument> findAllByCompanyId(ObjectId id);
 }
