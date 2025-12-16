de4e95dc0f64ecbafec8c163d8cea855e8f5fd9a
emailkolar <emilaalund.nyboder@gmail.com>
2025-12-16 11:24:19 +0100

    fixed cypress test & workflow dispatch

--- client/cypress/e2e/admin_login_and_add_and_delete_movie.cy.ts
+++ client/cypress/e2e/admin_login_and_add_and_delete_movie.cy.ts
@@ -1,33 +1,36 @@
 /// <reference types="cypress" />
-
+ 
 describe("Admin Login and delete movie", () => {
   beforeEach(() => {
     cy.visit("/");
   });
-
+ 
   it("should log in and log out successfully", () => {
+ 
+    cy.intercept('DELETE', '/api/movies/*').as('deleteMovie');
+    cy.intercept('GET', '/api/movies').as('getMovies');
     // Open login modal
     cy.contains("Login").click();
-
+ 
     // Select inside the modal ONLY
     cy.get(".chakra-modal__content-container").within(() => {
       cy.get("input").eq(0).type("EmailKolar");
       cy.get('input[type="password"]').type("ASB123...lol");
       cy.contains('button',"Sign in").click();
     });
-
+ 
     // Make sure modal closes
     cy.get(".chakra-modal__content-container").should("not.exist");
-
+ 
     // Verify login
     cy.contains("EmailKolar").should("be.visible");
-
+ 
     // Go to admin panel
     cy.contains("Go to admin").click();
-
+ 
     // Open modal
     cy.contains("Add movie").click();
-
+ 
     // Fill modal form
     cy.get(".chakra-modal__content-container").within(() => {
       cy.get('input').eq(0).type("Cypress Test Movie");                 // Title
@@ -36,27 +39,29 @@ describe("Admin Login and delete movie", () => {
       cy.get('input').eq(3).type("2025-01-01");                         // Release date
       cy.get('input').eq(4).type("120");                                // Runtime
       //cy.get('input').eq(5).type("");     // Poster URL
-
+ 
       cy.contains("button", "Create").click();
     });
-
+ 
     // Wait for modal to close + UI to update
     cy.get(".chakra-modal__content-container").should("not.exist");
-
+ 
     // Verify movie exists in list
     cy.contains("Cypress Test Movie", { timeout: 10000 }).should("be.visible");
-
+ 
     // Delete it
     cy.contains("Cypress Test Movie")
-      .parent()                     // go to its movie row
-      .within(() => {
-        cy.get('[aria-label="Delete movie"]').click();
-      });
-
+    .parent()
+    .parent()
+    .find('[aria-label="Delete movie"]')
+    .click();
+ 
+    cy.wait('@deleteMovie');
+    cy.wait('@getMovies');
+ 
     // Confirm it disappears
     cy.contains("Cypress Test Movie").should("not.exist");
-
-
+ 
+ 
   });
 });
-
